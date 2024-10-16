c69c64feaa06ebba42c37a2b7c26377ed56f6e4d
Capitaller <130102anton@gmail.com>
2024-10-16 21:40:21 +0200

    Update WorkoutSplitsView.swift

--- SmartTrainer/Core/Profile/Workouts/WorkoutSplitsView.swift
+++ SmartTrainer/Core/Profile/Workouts/WorkoutSplitsView.swift
@@ -8,22 +8,42 @@
 import SwiftUI
 
 struct WorkoutSplitsView: View {
+    @ObservedObject var healthViewModel: HealthViewModel
+    
     var body: some View {
         VStack {
-            Text("Workout Splits")
-                .font(.largeTitle)
-                .padding()
-            
-            // Add your logic to show workout splits here
-            List(0..<5) { index in
-                Text("Split \(index + 1)")
+            if healthViewModel.workoutSplits.isEmpty {
+                Text("No workout splits available.")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color(.darkGray))
+                    .padding(10)
+            } else {
+                Text("Generated Workout Splits")
+                    .font(.system(size: 20))
+                    .foregroundColor(Color(.darkGray))
+                    .padding(10)
+                ForEach(healthViewModel.workoutSplits) { split in
+                    VStack(alignment: .leading, spacing: 10) {
+                        // Block for each workout split
+                        Text("Split No: \(split.id)")
+                            .font(.headline)
+                        
+                        Text("Distance: \(split.distance, specifier: "%.2f")")
+                        Text("Split Speed: \(String(format: "%.2f", split.splitspeed))")
+                    }
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 40)
+                }
             }
         }
-        .navigationBarTitle("Workout Splits", displayMode: .inline)
+        .padding()
     }
 }
 
-#Preview {
-    WorkoutSplitsView()
-}
 
+
+//#Preview {
+//    WorkoutSplitsView()
+//}
