06a19c85251b070a3631c16eb6b77e5a3adb3ce6
Capitaller <130102anton@gmail.com>
2024-10-18 23:29:34 +0200

    Moving Text constants

--- SmartTrainer/Core/Health/ViewModel/HealthViewModel.swift
+++ SmartTrainer/Core/Health/ViewModel/HealthViewModel.swift
@@ -205,19 +205,9 @@ class HealthViewModel: ObservableObject {
     
     func generateUserJSON(workoutType: String, intensity: String, distance: Double, averageSpeedMS: Double, athleteID: String, age: Int, weight: Double) -> String {
         // Create the messages part
-        let systemMessage = """
-        {
-            "role": "system",
-            "content": "You are a personal fitness trainer. RETURN ONLY JSON WITH WORKOUTSPLITS. NO TEXT EXCEPT JSON. NO OTHER JSON PARTS EXCEPR WORKOUTSPLITS."
-        }
-        """
+        let systemMessage =  RoleMessages.systemMessage
 
-        let assistantMessage = """
-        {
-            "role": "assistant",
-            "content": "You are going to receive workout parameters such as Workout Type(Running, Cycling, Walking), Workout Intensity (Low/Medium/High), Desired workout distance (in km), athlete age, Weight and GENDER in JSON. You should return workout splits formatted in JSON file. You will get JSON with user data You should create new workoutsplits in this JSON AND RETURN ONLY workoutSplits. GENERATE UP TO 5 WORKOUT SPLITS RETURN ONLY workoutSplits JSON PART. every first split shuld be with lower intensity (warm-up). Generate different WORKOUT splits, you can change workout intensity, speed (meters per second) and distance for every new split, but the overall result should match general workout settings. DISTANCE IN EACH SPLIT CAN BE DIFFERENT BUT TOTAL DISTANCE IN ALL WORKOUT SPLITS SHOULD BE THE SAME AS MAXDISTANCE. SPLITINTENSITY CAN NOT BE MORE INTENSIVE THAN IN GIVEN intensity PARAMETR, BUT CAN BE LESS INTENSIVE. averageSpeedMS is an average speed in athlete workouts with the same type for several previous workouts. FOR LOW WORKOUT INTENSITY CREATE SPLITS WITH TOTAL AVERAGE speed LOWER THAN Value IN averageSpeedMS. FOR MEDIUM WORKOUT INTENSITY CREATE SPLITS WITH TOTAL AVERAGE speed SIMILAR TO Value IN averageSpeedMS.FOR HIGH WORKOUT INTENSITY CREATE SPLITS WITH TOTAL AVERAGE speed HIGHER THAN Value IN averageSpeedMS. DIFFERENCE IN SPEED FROM FASTEST TO LOWEST SPLIT SHOUD NOT BE MORE THAN 50% FROM GIVEN IN speed PARAMETR. DO NOT CREATE DIFFERENT SPLITS WITH THE SAME speed IN A ROW. SPLITS WITH THE SAME SPEED SHOUD HAVE THE SAME INTENSITY. RETURN ONLY workoutSplits JSON PART. NO TEXT EXCEPT JSON."
-        }
-        """
+        let assistantMessage = RoleMessages.assistantMessage
 
         // Create the user message
         let userMessage = """
--- SmartTrainer/Model/RoleMessages.swift
+++ SmartTrainer/Model/RoleMessages.swift
@@ -9,11 +9,17 @@ import Foundation
 
 struct RoleMessages {
     static let systemMessage = """
-    You are a personal fitness trainer. You are going to receive workout parameters such as Workout Type (Running, Cycling, Walking), Workout Intensity (Low/Medium/High), Desired workout distance (in km), athlete age, VO2Max, and Weight in JSON. You should return workout splits formatted in JSON file. RETURN ONLY JSON. NO TEXT EXCEPT JSON. You will get JSON with user data. You should add new training splits in this JSON as in the example. GENERATE UP TO 5 TRAINING SPLITS. RETURN ONLY \"workoutSplits\" JSON PART:
-    """
+        {
+            "role": "system",
+            "content": "You are a personal fitness trainer. RETURN ONLY JSON WITH WORKOUTSPLITS. NO TEXT EXCEPT JSON. NO OTHER JSON PARTS EXCEPR WORKOUTSPLITS."
+        }
+        """
     
     static let assistantMessage = """
-    Generate different training splits, you can change workout intensity, time in seconds and distance for every new split, but the overall result should match general workout settings. RETURN ONLY \"workoutSplits\" JSON. TOTAL DISTANCE IN ALL TRAINING SPLITS SHOULD BE THE SAME AS MAXDISTANCE
-    """
+        {
+            "role": "assistant",
+            "content": "You are going to receive workout parameters such as Workout Type(Running, Cycling, Walking), Workout Intensity (Low/Medium/High), Desired workout distance (in km), athlete age, Weight and GENDER in JSON. You should return workout splits formatted in JSON file. You will get JSON with user data You should create new workoutsplits in this JSON AND RETURN ONLY workoutSplits. GENERATE UP TO 5 WORKOUT SPLITS RETURN ONLY workoutSplits JSON PART. every first split shuld be with lower intensity (warm-up). Generate different WORKOUT splits, you can change workout intensity, speed (meters per second) and distance for every new split, but the overall result should match general workout settings. DISTANCE IN EACH SPLIT CAN BE DIFFERENT BUT TOTAL DISTANCE IN ALL WORKOUT SPLITS SHOULD BE THE SAME AS MAXDISTANCE. SPLITINTENSITY CAN NOT BE MORE INTENSIVE THAN IN GIVEN intensity PARAMETR, BUT CAN BE LESS INTENSIVE. averageSpeedMS is an average speed in athlete workouts with the same type for several previous workouts. FOR LOW WORKOUT INTENSITY CREATE SPLITS WITH TOTAL AVERAGE speed LOWER THAN Value IN averageSpeedMS. FOR MEDIUM WORKOUT INTENSITY CREATE SPLITS WITH TOTAL AVERAGE speed SIMILAR TO Value IN averageSpeedMS.FOR HIGH WORKOUT INTENSITY CREATE SPLITS WITH TOTAL AVERAGE speed HIGHER THAN Value IN averageSpeedMS. DIFFERENCE IN SPEED FROM FASTEST TO LOWEST SPLIT SHOUD NOT BE MORE THAN 50% FROM GIVEN IN speed PARAMETR. DO NOT CREATE DIFFERENT SPLITS WITH THE SAME speed IN A ROW. SPLITS WITH THE SAME SPEED SHOUD HAVE THE SAME INTENSITY. RETURN ONLY workoutSplits JSON PART. NO TEXT EXCEPT JSON."
+        }
+        """
 }
 
