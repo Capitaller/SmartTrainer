d17c586d35396d090a762019c2a98c82f5e81dbd
Capitaller <130102anton@gmail.com>
2024-11-02 19:03:28 +0100

    Update Watch Connectivity

--- SmartTrainer Watch App/IOS Connectivity/WatchConnectivity.swift
+++ SmartTrainer Watch App/IOS Connectivity/WatchConnectivity.swift
@@ -8,16 +8,18 @@
 import Foundation
 import WatchConnectivity
 
+
 class WatchConnectivityManager: NSObject, WCSessionDelegate, ObservableObject {
     var session: WCSession
-
+    var dbManager = DBManager()
+    
     override init() {
         self.session = WCSession.default
         super.init()
         self.session.delegate = self
         self.session.activate()
     }
-
+    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         if let error = error {
             print("WCSession activation failed: \(error.localizedDescription)")
@@ -25,20 +27,25 @@ class WatchConnectivityManager: NSObject, WCSessionDelegate, ObservableObject {
             print("WCSession activated with state: \(activationState)")
         }
     }
-
-    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-        print("message")
-        print(message)
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String: Any]) {
+        print("Received message:", message)
         guard let workoutSplit = parseWorkoutSplit(from: message) else {
             print("Failed to parse workout split.")
             return
         }
-
-        // Here, you would typically save the workout split to your data store
-        // For demonstration purposes, we'll just print it
-        print("Received workout split: \(workoutSplit)")
+        
+        // Clear existing workout splits and add the new one
+        let db = DBManager()
+        
+        if workoutSplit.id == 1 {
+            db.createWorkoutSplitsTable()
+            
+            db.deleteAllWorkoutSplits()
+        }// Clear previous data
+        db.insertWorkoutSplit(workoutSplit: workoutSplit)  // Insert new data
     }
-
+    
     private func parseWorkoutSplit(from message: [String: Any]) -> WorkoutSplit? {
         guard let id = message["id"] as? Int,
               let workoutId = message["workoutId"] as? Int,
@@ -50,7 +57,7 @@ class WatchConnectivityManager: NSObject, WCSessionDelegate, ObservableObject {
               let comment = message["comment"] as? String else {
             return nil
         }
-
         return WorkoutSplit(id: id, workoutId: workoutId, athleteID: athleteID, distance: distance, splitspeed: splitspeed, splitIntensity: splitIntensity, splitworkouttype: splitworkouttype, comment: comment)
     }
 }
+
